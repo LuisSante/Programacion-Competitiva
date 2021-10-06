4aa6a7d865204f0e9db6acf1004266d52e2576bf
LuisSante <lsantet@unsa.edu.pe>
2021-10-05 21:11:02 -0500

    Nuevo

--- Ejercicios 30-09-2021/sliding_cost.cpp
+++ Ejercicios 30-09-2021/sliding_cost.cpp
@@ -68,8 +68,8 @@ int main(){
 //100% CASES
 
 #include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>  
-#include <ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>  
+#include<ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
 using namespace __gnu_pbds;
