46bce5e42d1a753d6692df38fb88f876b20809b3
LuisSante <lsantet@unsa.edu.pe>
2021-10-21 12:15:47 -0500

    Ejercicios 20-10-2021

--- Ejercicios 20-10-2021/score.cpp
+++ Ejercicios 20-10-2021/score.cpp
@@ -6,9 +6,14 @@ int score(string s) {
     stack<char> paren;
     int cont = 0;
     for(int i = 0; i < s.size(); i++){
-       if(s[i] == '('){
-           paren.push(s[i]);
-       }
+        if(s[i] == '('){
+           paren.push(cont);
+           cont=0;
+        }
+        else if(s[i] == ')'){
+            cont = paren.top()+max(cont*2,1);
+            paren.pop();
+        }
     }
     return cont;
 }
@@ -17,6 +22,7 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-
+    string s = "(()(()))";
+    cout<<score(s);
     return 0;
 }
