0276e527f5df29a06c8c435a147969828ae1745d
LuisSante <lsantet@unsa.edu.pe>
2021-11-10 07:44:23 -0500

    Avances

--- Segunda Unidad/Ejercicios 27-10-2021/interview.cpp
+++ Segunda Unidad/Ejercicios 27-10-2021/interview.cpp
@@ -1,14 +1,33 @@
-#include<iostream>
+#include<cstdio>
+#include<cstring>
 #include<algorithm>
-#include<vector>
-#include<iterator>
+#include<iostream>
 #include<queue>
+#include<vector>
 
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+void solution(vector<int>& arr){
+    deque<int> d;
+    for(auto value:arr) d.push_back(value);
+    int res = 0;
 
+    while(d.front() != -1 && d.back() != -1){
+        if(d.front() < d.back()){
+            res += d.front();
+            d.pop_front();
+        }
+
+        else{
+            res += d.back();
+            d.pop_back();
+        }
+    }
+    cout<<res<<endl;
+}
+
+int main(){
+    vector<int> A = {4,-1,5,2,3};
+    solution(A);
     return 0;
-}    
+}
